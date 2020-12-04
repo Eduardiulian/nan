6d7d36453e495c892b867b89f0c7dc19968644f7
LaptopSSDacc <fzastahov@gmail.com>
2020-12-04 16:51:40 +0300

    chat controller + json

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,13 @@
 class ChatsController < ApplicationController
-  def chat
-  end
+	def chat
+	end
+
+	def refresh
+		respond_to do |format|
+			msgs = Message.all
+			format.json do
+				render json: msgs.to_json
+			end
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]  
   post 'invites/accept'
   get 'chats/chat'
+  get 'chats/refresh'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
