fe73c6bbe5752a3181f90f7003cf8d543f96b95f
LaptopSSDacc <fzastahov@gmail.com>
2020-12-08 21:08:54 +0300

    ajax msgs proto

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,14 @@
 class ChatsController < ApplicationController
 	def chat
+		# @target_email = params[:target_email]
+		# logger.info("~log_target: " + @target_email)
 	end
 
 	def refresh
+		@target_email = params[:target_email]
+	    logger.info("~log_target_refresh: " + @target_email.to_s)
+	    msgs = Message.all
 		respond_to do |format|
-			msgs = Message.all
 			format.json do
 				render json: msgs.to_json
 			end
